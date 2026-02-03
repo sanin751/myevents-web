25b4a5f91ea047808be5fa1c6adc3455a48eebf4
Sanin <sanin9841393780@gmail.com>
2026-02-03 17:40:37 +0545

    updated endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -2,5 +2,11 @@ export const API = {
     AUTH: {
         LOGIN: '/api/auth/login',
         REGISTER: '/api/auth/register',
+        UPDATEPROFILE: '/api/auth/update',
+    },
+    ADMIN:{
+        USER:{
+            CREATE: '/api/admin/users/',
+        }
     }
 }
