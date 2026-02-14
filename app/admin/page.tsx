dd0a3c2a20f7c64719be4ced68adb16e41f85b79
Sanin <sanin9841393780@gmail.com>
2026-02-14 21:27:51 +0545

    updated pages

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,33 @@
-export default function Page() {
+import Link from "next/link";
+import { handleGetAllUsers } from "@/lib/actions/admin/user-action";
+import UserTable from "./users/_components/UserTable";
+
+
+export default async function Page({
+    searchParams
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+    const params = await searchParams;
+    const page = params.page as string || '1';
+    const size = params.size as string || '10';
+    const search = params.search as string || '';
+
+    const response = await handleGetAllUsers(
+        page,
+        size,
+        search as string
+    );
+
+    if (!response.success) {
+        throw new Error(response.message || 'Failed to load users');
+    }
+
     return (
-        <div>Dashboard Here</div>
+        <div className="p-2">
+            <Link className="text-blue-500 border border-blue-500 p-2 rounded inline-block"
+                href="/admin/users/create">Create User</Link>
+            <UserTable users={response.data} pagination={response.pagination} search={search} />
+        </div>
     );
 }
